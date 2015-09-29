69a9c17cd88564a25ceed0502abf03cde4773593
Kevin Chilcott <kevin.chilcott@siftmedia.co.uk>
2015-09-29 15:20:15 +0100

    index.php file for my idd_scripting_template unit

---
+++ index_idd_template.php
@@ -0,0 +1,13 @@
+<html>
+        <head>
+        </head>
+        <body>
+                <?php
+                    echo '<p>this is some php</p>';
+					echo '<p>Here is a <a href="form.php">link</a> to the form example</p>';
+					echo '<p>Here is a <a href="info.php">link</a> to see the power of a php function... we will go into that next time!</p>';
+                	var_dump($_GET);
+                    echo phpinfo();
+                ?>
+        </body>
+</html>
